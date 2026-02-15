92f5ca3601c45fd51308faf73e26f7a426e558b6
Christophe Blaess <christophe.blaess@logilin.fr>
2026-02-15 06:58:54 +0100

    initscripts: don't write message on console.

---
+++ meta-eris-linux/recipes-core/initscripts/initscripts/banner.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+### BEGIN INIT INFO
+# Provides: banner
+# Required-Start:
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+### END INIT INFO
+
+if [ ! -e /dev/tty ]; then
+    /bin/mknod -m 0666 /dev/tty c 5 0
+fi
